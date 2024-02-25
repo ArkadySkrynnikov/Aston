25e02afb4ef7a3fda8c641b19ed8334ae2435f51
ArkadySkrynnikov <catmaster1488@gmail.com>
2024-02-25 15:08:28 +0700

    ASTONJS-1 [fix]: refactor context, refactor bind, refactor array

--- bindPolyfill.ts
+++ bindPolyfill.ts
@@ -7,15 +7,29 @@ function log(string: string): void {
 }
 
 interface Function {
-    bindPolyfill(obj: Record<any, any>, ...args: any[]): Function;
+    bindPolyfill(obj: Record<any, any>, ...args: any[]): Function
 }
 
 Function.prototype.bindPolyfill = function(obj: any, ...args: any[]): Function {
     let fn = this;
+
     return function (...argsAfterContext: any[]) {
-        fn.apply(obj, [...args, ...argsAfterContext]);
-    };
+        const key = Date.now().toString() + Math.random().toString();
+
+        obj[key] = fn;
+
+        const res = obj[key](...args,...argsAfterContext);
+
+        delete obj[key];
+
+        return res;
+    }
+    // let fn = this;
+    // return function (...argsAfterContext: any[]) {
+    //     fn.apply(obj, [...args, ...argsAfterContext]);
+    // };
 };
 
 log.bindPolyfill(context)("string")
 log.bindPolyfill(context, "string")()
+console.log(context)
--- context.ts
+++ context.ts
@@ -6,6 +6,6 @@ function logger(): void {
     console.log(`I output only external context: ${this.item}`)
 }
 
-logger.bind(obj,[])()
-logger.apply(obj,[])
+logger.bind(obj)()
+logger.apply(obj)
 logger.call(obj)
