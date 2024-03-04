e3a045dad081f2768e8785c109004bca96a50a5a
ArkadySkrynnikov <catmaster1488@gmail.com>
2024-03-04 19:33:10 +0700

    ASTONJS-1 [feat]: refactor indexTimeout

--- indexTimeout.ts
+++ indexTimeout.ts
@@ -1,9 +1,21 @@
-const indexTimeout = (arr: number[]): void => {
+// const indexTimeout = (arr: number[]): void => {
+//     for (let i = 0; i < arr.length; i++) {
+//         setTimeout(function() {
+//             console.log(i);
+//         }, 3000);
+//     }
+// }
+
+const indexTimeout = (arr: number[]) : void => {
     for (let i = 0; i < arr.length; i++) {
-        setTimeout(function() {
-            console.log(i);
-        }, 3000);
+        timeoutLog(i)
     }
+}
+
+function timeoutLog(i: number){
+    setTimeout(()=>{
+        console.log(i)
+    },3000*i)
 }
 
 indexTimeout([-2, -1, 0, 1, 2])
